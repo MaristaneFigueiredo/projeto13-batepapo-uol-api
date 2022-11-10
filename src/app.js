ccc14ddf7db0b447b816d79aa8ef7c4db4245e0a
MaristaneFigueiredo <maristanetecnologia@gmail.com>
2022-11-10 10:27:18 -0300

    fix: ajustes

--- src/app.js
+++ src/app.js
@@ -4,20 +4,27 @@ import cors from "cors"
 import joi from "joi"
 // biblioteca do mongodb
 import {MongoClient} from "mongodb"
+import dotenv from "dotenv"
 
 //config
 const app = express();
 app.use(cors());
 app.use(express.json()) // qro receber requisições via body no formato json
-const mongoClient = new MongoClient("mongodb://localhost:27017");
-let db;
 
-// aqui gera uma promessa
-mongoClient.connect()
+dotenv.config() // configura as variáveis de ambiente
+//const mongoClient = new MongoClient("mongodb://localhost:27017");
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db; //variável do banco atual
+
+
+// conectando a base de dados
+mongoClient
+.connect()
 .then(() => {
     db = mongoClient.db("batePapo")
 })
-.catch( (error) => console.log(error));
+.catch((error) => console.log(error));
+
 
 //validações
 const validaçoesParticipants = joi.object({
@@ -25,6 +32,8 @@ const validaçoesParticipants = joi.object({
             .required()
 })
 
+
+// POST Participants
 app.post("/participants", (req, res) => {
     const {name} = req.body
 
@@ -32,43 +41,51 @@ app.post("/participants", (req, res) => {
     if (error) {
         res.status(422).send({error: error.details})
         return
-        //return res.status(422).send({error:'O nome é obrigatório'})
+        //422: Unprocessable Entity => Significa que a requisição enviada não está no formato esperado
     }
 
 
-    const isParticipantExists = db.collection("participants").find({name:name })
+    const isParticipantExists = db.collection("participants").find({name:name})
 
     if (isParticipantExists) {
         res.status(409).send({error: "Participante já existe."})
         return
+        //409: Conflict => Significa que o recurso que você está tentando inserir já foi inserido    
     }
 
     // salvar no mongodb
-    db.collection("participants").insert({name:name, lastStatus: Date.now() }).then( (response) => {
+    db.collection("participants").insert({name:name, lastStatus:Date.now()})
+    .then( (response) => {
         // o response é a resposta do banco
         console.log(response)
         // esse res é o res da rota          
-        res.status(201) }
+        res.sendStatus(201)
+        //201: Created => Sucesso na criação do recurso
+     }
     ).catch((error) => {
         res.status(500).send(error)
+        //500: Internal Server Error => Significa que ocorreu algum erro desconhecido no servidor
     })
     
 
  
 })
 
-
+// GET Participants
 app.get("/participants", (req, res) =>{
-    db.collection("participants").find().toArray().then( (participants) => {        
+    db.collection("participants").find().toArray()
+    .then( (participants) => {        
         res.send(participants)
-    }) .catch((err) => {
-        console.log(err)
-        res.sendStatus(500)
+    }) .catch((error) => {
+        console.log(error)
+        res.status(500).send(error)
     })
-
     
 })
 
+
+
+
 app.listen(5000, () => console.log("Server running in port 5000"))
 
-//41
+//dotenv - gerencia variáveis de ambiente e protege os dados
