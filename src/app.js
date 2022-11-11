82d26e539696359261610783773d8df384b2e83b
Maristane Figueiredo <maristanetecnologia@gmail.com>
2022-11-11 14:14:16 -0300

    feat: Get messages pronto

--- src/app.js
+++ src/app.js
@@ -11,14 +11,14 @@ const app = express();
 app.use(cors());
 app.use(express.json()); // qro receber requisições via body no formato json
 dotenv.config(); // configura as variáveis de ambiente
-const mongoClient = new MongoClient(process.env.MONGO_URI);
+const conectionDataBase = new MongoClient(process.env.MONGO_URI);
 let db; //variável do banco atual
 
 // conectando a base de dados
 // top-level await - await fora de funções. Funciona com o sistema de modules - (esmodules)
 try {
-  await mongoClient.connect();
-  db = mongoClient.db("batePapo");
+  await conectionDataBase.connect();
+  db = conectionDataBase.db("batePapoUol");
 } catch (error) {
   console.log(error);
 }
@@ -31,16 +31,10 @@ const participantSchema = joi.object({
 const messageSchema = joi.object({
   to: joi.string().required(),
   text: joi.string().required(),
-  type: joi.allow(["message", "private_message"]),
+  type: joi.any().valid("message", "private_message") // permite um ou outro
 });
 
-//
-let isParticipantExists;
-function participantExists(participant) {
-  return (isParticipantExists = db
-    .collection("participants")
-    .find({ name: participant }));
-}
+
 
 // POST Participants
 app.post("/participants", async (req, res) => {
@@ -48,32 +42,26 @@ app.post("/participants", async (req, res) => {
 
   const validation = participantSchema.validate({ name });
 
-  if (validation.error) {
-    console.log(validation.error.details.message);
+  if (validation.error) {    
     res.status(422).send({ error: validation.error.details.message });
     //422: Unprocessable Entity => Significa que a requisição enviada não está no formato esperado
   }
 
-  /*const isParticipantExists = db.collection("participants").find({name:name})
-    if (isParticipantExists) {
+  const isParticipantExists =( await db.collection("participants").find({name:name}).toArray()  ).length > 0  
+  if (isParticipantExists) {
         res.status(409).send({error: "Participante já existe."})
         return
         //409: Conflict => Significa que o recurso que você está tentando inserir já foi inserido    
-    }*/
+    }
 
-  if (participantExists(name)) {
-    res.status(409).send({ error: "Participante já existe." });
-    return;
-    //409: Conflict => Significa que o recurso que você está tentando inserir já foi inserido
-  }
+
 
   try {
-    await db
-      .collection("participants")
-      .insert({ name: name, lastStatus: Date.now() });
+    await db.collection("participants").insertOne({ name: name, lastStatus: Date.now() });
+    await db.collection("messages").insert({ from: name, to:'Todos', text:'entra na sala...', type:'status', time:dayjs().format('HH:mm:ss')})
     res.sendStatus(201);
     //201: Created => Sucesso na criação do recurso
-  } catch (error) {
+  } catch (error) {  
     res.status(500).send(error);
     //500: Internal Server Error => Significa que ocorreu algum erro desconhecido no servidor
   }
@@ -93,25 +81,24 @@ app.get("/participants", async (req, res) => {
 app.post("/messages", async (req, res) => {
   const {to, text, type} = req.body;
   const from = req.headers.user;
-  /*const posts = getPostsThatThisUserMightLike(user);
-	res.send(posts);*/
-  /*  const extratoDoCliente = extrato.filter((mov) => mov.cliente === usuario);
-  console.log(extratoDoCliente); */
 
-  const validation = messageSchema.validate({to, text, type}, { abortEarly: false });
+
+  const validation = messageSchema.validate({to, text, type}, { abortEarly: false }   );
   if (validation.error) {
     const errors = validation.error.details.map((detail) => detail.message)
     res.status(422).send(errors);
     return;
   }
 
-  if (!participantExists(from)) {
-    res.send({ error: "Participante não existe!" });
-    return;
-  }
+  const isParticipantExists =( await db.collection("participants").find({name:from}).toArray()  ).length > 0  
+  if (!isParticipantExists) {
+        res.status(409).send({error: "Participante não existe!"})
+        return
+        //409: Conflict => Significa que o recurso que você está tentando inserir já foi inserido    
+    }
 
   try {
-    await db.collection("messages").insert({ from: from, to:to, text:text, type:type, time:dayjs().format('HH:MM:SS')})
+    await db.collection("messages").insert({ from: from, to:to, text:text, type:type, time:dayjs().format('HH:mm:ss')})
     res.sendStatus(201);
   } catch (error) {
     res.status(500).send(error);
@@ -121,8 +108,27 @@ app.post("/messages", async (req, res) => {
 
 //GET Messages
 app.get("/messages", async (req, res) => {
+  const limit = parseInt(req.query.limit)
+  const user = req.headers.user;
+
+  /*if (limit !== 0) {
+    const tenLastTweets = tweetsReturn.slice(-10)
+  } else {
+    try {
+      const messages = await db.collection("participants").find({from: user, to:user}).toArray()
+      res.send(messages);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  }*/
+  
+  
   try {
-  } catch (error) {}
+    const messages = await db.collection("participants").find({from: user, to:user}).toArray().reverse()
+    res.send(messages);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 app.listen(5000, () => console.log("Server running in port 5000"));
